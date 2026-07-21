575d0044b80faff556e05ca9e5fed7eca163196c
innowavesolution <innowavesolutioninfo@gmail.com>
2026-07-22 03:52:21 +0600

    update cors

--- apps/console/next.config.js
+++ apps/console/next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  basePath: '/console',
   
   // Reduce bundle size
   compiler: {
